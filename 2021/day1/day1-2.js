05a511938ad9cf0be2107401ad29305f50cbbf34
dreamxinxcode <brandon@dreamincode.dev>
2021-12-04 18:31:41 -0800

    Day 1 part 2

---
+++ 2021/day1/day1-2.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+let input = fs.readFileSync('./input.txt', 'utf8')
+    .toString()
+    .trim()
+    .split('\n')
+    .map((num) => parseInt(num, 10));
+
+let answer = 0;
+let prev;
+let sum;
+
+for (let i = 0; i < input.length; i++) {
+    const items = input.slice(i, i + 3);
+    if (!items.length) {
+        break;
+    }
+    sum = items.reduce((a, b) => a + b);
+    if (prev && sum > prev && i > 0) {
+        answer++;
+    }
+    prev = sum;
+}
+
+console.log(answer)
